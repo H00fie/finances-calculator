c11ebf893aba0110898e141cff874c924330dabd
Dazikiri Anikar <mierzwinski.b@gmail.com>
2020-07-24 07:51:41 +0200

    added exception handling mechanism and test for it

--- src/main/java/bm/app/services/VatService.java
+++ src/main/java/bm/app/services/VatService.java
@@ -13,12 +13,15 @@ public class VatService {
         this.vatValue = new BigDecimal("0.23");
     }
 
-    public BigDecimal getGrossPriceForDefaultVat(AdditionalMiniProduct additionalMiniProduct){
+    public BigDecimal getGrossPriceForDefaultVat(AdditionalMiniProduct additionalMiniProduct) throws Exception {
         return getGrossPrice(additionalMiniProduct.getNetPrice(), vatValue);
     }
 
-    public BigDecimal getGrossPrice(BigDecimal netPrice, BigDecimal vatValue){
+    public BigDecimal getGrossPrice(BigDecimal netPrice, BigDecimal vatValue) throws Exception {
         MathContext mathContext = new MathContext(5);
+        if (vatValue.compareTo(BigDecimal.ONE) == 1){
+            throw new Exception("VAT has to be lower than one.");
+        }
         return netPrice.multiply(vatValue.add(BigDecimal.ONE)).round(mathContext);
     }
 }
--- src/test/java/bm/app/services/VatServiceTest.java
+++ src/test/java/bm/app/services/VatServiceTest.java
@@ -2,6 +2,7 @@ package bm.app.services;
 
 import bm.app.models.AdditionalMiniProduct;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -13,7 +14,8 @@ class VatServiceTest {
     VatService vatService;
 
     @Test
-    void shouldCalculateGrossPriceForDefaultVat() {
+    @DisplayName("Should calculate the gross price for the default VAT value.")
+    void shouldCalculateGrossPriceForDefaultVat() throws Exception {
         //given
         AdditionalMiniProduct product = generateProductWithPrice("400.00");
         //when
@@ -24,7 +26,8 @@ class VatServiceTest {
     }
 
     @Test
-    void shouldCalculateGrossPriceForDifferentVat() {
+    @DisplayName("Should calculate the gross price for a different VAT value.")
+    void shouldCalculateGrossPriceForDifferentVat() throws Exception {
         //given
         AdditionalMiniProduct product = generateProductWithPrice("100");
         //when
@@ -33,8 +36,19 @@ class VatServiceTest {
         assertEquals(new BigDecimal("108.00"), result);
     }
 
-    private AdditionalMiniProduct generateProductWithPrice(String vat){
-        return new AdditionalMiniProduct(UUID.randomUUID(), new BigDecimal(vat));
+    @Test
+    @DisplayName("Should throw Exception when VAT value is too high.")
+    void shouldThrowExceptionWhenVatIsTooHigh(){
+        //given
+        AdditionalMiniProduct additionalMiniProduct = generateProductWithPrice("1000.00");
+        //then
+        assertThrows(Exception.class, () -> {
+            vatService.getGrossPrice(additionalMiniProduct.getNetPrice(), BigDecimal.TEN);
+        });
+    }
+
+    private AdditionalMiniProduct generateProductWithPrice(String price){
+        return new AdditionalMiniProduct(UUID.randomUUID(), new BigDecimal(price));
     }
 
     @BeforeEach
